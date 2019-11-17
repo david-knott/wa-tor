fc730d59fe3220da31408f87325ddd0a850df92f
April McDonnell <mrdavidknott@gmail.com>
2019-11-17 17:18:43 +0000

    ts debug

--- webpack.config.js
+++ webpack.config.js
@@ -2,7 +2,7 @@ const path = require('path');
 
 module.exports = {
     entry: './src/index.js',
-    devtool: 'source-map',
+    devtool: 'inline-source-map',
     output: {
         filename: 'main.js',
         path: path.resolve(__dirname, 'dist'),
